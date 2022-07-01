9769c55bd7c72743ed575d31e14f4b0d6564d7f9
uzb379 <abdurahmonochilov7@gmail.com>
2022-07-01 20:53:47 +0500

    homework

--- js/masala.js
+++ js/masala.js
@@ -1,4 +1,4 @@
-const obj = {
+const myMap = {
     Water: '$130', 
     Bread: '$230',
     TV: '$450',
@@ -6,6 +6,16 @@ const obj = {
     Phone: '$350',
     Notebook: '$870',
 }
+function productName(obj, price){
+    const arr = []
+    const keys = Object.keys(obj)
+    for (const key in obj){
+        const values =obj[key];
+        if(price>= values){
+            arr.push(keys)
+        }
+    }
+}
 
 
-console.log('uzur ustoz ulgurmadim');
+console.log(productName(myMap, 300))
