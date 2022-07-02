67b820cdfa67853211d56c18e0d24100228115bd
uzb379 <abdurahmonochilov7@gmail.com>
2022-07-02 12:22:39 +0500

    homework

--- js/auth.js
+++ js/auth.js
@@ -1,121 +1,113 @@
 const URL = "https://n36-todolist.herokuapp.com";
-const signUpLink = document.querySelector(".sign-up__link");
-const signInLink = document.querySelector(".sign-in__link");
-//Sign UP
-const elSignUpModal = document.querySelector(".sign-up");
-const elSignUpName = document.querySelector(".sign-up__user-name");
-const elSignUpPassword = document.querySelector(".sign-up__user-password");
-//Sign IN
 
-const elLoginModal = document.querySelector(".sign-in");
-const elLoginName = document.querySelector(".sign-in__user-name");
-const elLoginPassword = document.querySelector(".sign-in__user-password");
+const token = JSON.parse(localStorage.getItem("token"));
+const elToDoList = document.querySelector(".todo__list");
+const elToDoTemplate = document.querySelector(".template").content;
+const elToDoButton = document.querySelector(".add-todo__btn");
+const elToDoForm = document.querySelector(".add-todo");
+const elToDoInput = document.querySelector(".add-todo__input");
+const elLogOut = document.querySelector('.log-out')
 
-//visibiltyON
-const elToggle = document.querySelector("#toggle-password");
-const elClosed = document.querySelector(".first");
-const elOpen = document.querySelector(".second");
-const elInput = document.querySelector("#password");
-
-let isPasswordOn = true;
-
-elToggle.addEventListener("click", function () {
-  if (!isPasswordOn) {
-    elInput.type = "text";
-    elClosed.classList.add("hide");
-    elOpen.classList.remove("hide");
-  } else {
-    elInput.type = "password";
-    elClosed.classList.remove("hide");
-    elOpen.classList.add("hide");
-  }
-  isPasswordOn = !isPasswordOn;
-});
-//VisibilityIN
-const elToggleIn = document.querySelector("#toggle-password2");
-const elClosedIn = document.querySelector(".eye-off");
-const elOpenIn = document.querySelector(".eye-on");
-const elInputIn = document.querySelector("#password1");
-
-let isPasswordOnIn = true;
-
-elToggleIn.addEventListener("click", function () {
-  if (!isPasswordOnIn) {
-    elInputIn.type = "text";
-    elClosedIn.classList.add("hidden");
-    elOpenIn.classList.remove("hidden");
-  } else {
-    elInputIn.type = "password";
-    elClosedIn.classList.remove("hidden");
-    elOpenIn.classList.add("hidden");
-  }
-  isPasswordOnIn = !isPasswordOnIn;
-});
-
-
-signUpLink.addEventListener("click", (e) => {
-  e.preventDefault();
-  document.body.classList.remove("auth-body--sign-up");
-  document.body.classList.add("auth-body--sign-in");
-});
-signInLink.addEventListener("click", (e) => {
-  e.preventDefault();
-  document.body.classList.add("auth-body--sign-up");
-  document.body.classList.remove("auth-body--sign-in");
-});
-(function () {
-  fetch("https://n36-todolist.herokuapp.com");
-});
-
-//SIGN UP
-async function Register(user) {
-  const response = await fetch(`${URL}/signup`, {
+//ADD TO DO
+async function addToDo(user) {
+  const response = await fetch(`${URL}/todos`, {
     method: "POST",
     headers: {
       "Content-type": "application/json",
+      token,
     },
     body: JSON.stringify(user),
   });
-  const data = await response.json();
-  if (data.token) {
-    localStorage.setItem("token", JSON.stringify(data.token));
-    location.pathname = "index.html";
-  } else {
-    location.pathname = "auth.html";
-  }
 }
 
-elSignUpModal.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const data = {
-    userName: elSignUpName.value.trim(),
-    userPassword: elSignUpPassword.value.trim(),
-  };
-  Register(data);
-});
-
-//LOGIN
-async function Login(user) {
-  const response = await fetch(`${URL}/login`, {
-    method: "POST",
+elLogOut.addEventListener("click", (e)=>{
+  e.preventDefault
+  localStorage.removeItem("token")
+    location.pathname = "auth.html"
+})
+async function toDoRender() {
+  const response = await fetch(`${URL}/todos`, {
+    method: "GET",
     headers: {
       "Content-type": "application/json",
+      token,
     },
-    body: JSON.stringify(user),
   });
   const data = await response.json();
-  if (data.token) {
-    localStorage.setItem("token", JSON.stringify(data.token));
-    location.pathname = "index.html";
-  } else if ((data.token = undefined)) {
-    location.pathname = "auth.html";
-  }
+  RenderData(data);
 }
-elLoginModal.addEventListener("submit", (e) => {
+toDoRender();
+elToDoForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const data = {
-    login: elLoginName.value.trim(),
-    password: elLoginPassword.value.trim(),
+    text: elToDoInput.value.trim(),
   };
-  Login(data);
+  addToDo(data);
+});
+
+//Read
+async function toDoRender() {
+  const response = await fetch(`${URL}/todos`, {
+    method: "GET",
+    headers: {
+      "Content-type": "application/json",
+      token,
+    },
+  });
+  const data = await response.json();
+  console.log(data);
+  RenderData(data);
+}
+toDoRender();
+//Render DATA
+function RenderData(argument) {
+  elToDoList.innerHTML = null;
+  argument.forEach((element) => {
+    const newTemp = elToDoTemplate.cloneNode(true);
+    newTemp.querySelector(".todo__item").setAttribute("data-id", element.id);
+    newTemp.querySelector(".todo__text").textContent = element.body;
+    if (element.is_checked === true) {
+      newTemp.querySelector(".todo__checkbox").setAttribute("checked", true);
+    }
+    elToDoList.append(newTemp);
+  });
+}
+
+//DELETE
+async function deleteToDo(id) {
+  const response = await fetch(`${URL}/todos/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-type": "application/json",
+      token: token,
+    },
+  });
+  toDoRender();
+}
+elToDoList.addEventListener("click", (e) => {
+  e.preventDefault;
+  if (e.target.matches(".todo__btn")) {
+    const deleteId = e.target.closest(".todo__item").dataset.id;
+    deleteToDo(deleteId);
+  }
+});
+
+//EDIT
+async function editToDo(id) {
+  const response = await fetch(`${URL}/todos/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-type": "application/json",
+      token: token,
+    },
+  });
+  toDoRender();
+}
+
+elToDoList.addEventListener("change", (e) => {
+  e.preventDefault;
+  if (e.target.matches(".todo__checkbox")) {
+    const editId = e.target.closest(".todo__item").dataset.id;
+    editToDo(editId);
+  }
 });
